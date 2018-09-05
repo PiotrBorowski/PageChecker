d0c3addb40f2e3ad9d48579795a3aaa5cb9a9cf9
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2018-09-05 15:12:31 +0200

    zainstaloawny automapper

---
+++ PageChecker/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using PageCheckerAPI.DTOs.Page;
+using PageCheckerAPI.Models;
+
+namespace PageCheckerAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Page, PageDto>();
+        }
+    }
+}

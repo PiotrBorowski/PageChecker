517ed797f7dfd2825b983b04477b5dd463214e3d
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2018-09-05 14:00:07 +0200

    struktura projektu

---
+++ PageChecker/DTOs/Page/PageDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PageCheckerAPI.DTOs.Page
+{
+    public class PageDto
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public string Url { get; set; }
+    }
+}
---
+++ PageChecker/Repositories/Interfaces/IPageRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PageCheckerAPI.DTOs.Page;
+
+namespace PageCheckerAPI.Repositories.Interfaces
+{
+    public interface IPageRepository
+    {
+        List<PageDto> GetPages();
+        bool AddPage(PageDto pageDto);
+    }
+}

1485a98e64cf53f82f5c27299651d319a0851e5e
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2018-11-07 21:54:24 +0100

    max length of Name is 100 characters

--- PageChecker/DTOs/Page/AddPageDto.cs
+++ PageChecker/DTOs/Page/AddPageDto.cs
@@ -9,6 +9,7 @@ namespace PageCheckerAPI.DTOs.Page
 {
     public class AddPageDto
     {
+        [StringLength(100)]
         public string Name { get; set; }
         public int UserId { get; set; }
         [Required(ErrorMessage = "Refresh Rate is required")]
--- pagecheckerspa/src/constants.js
+++ pagecheckerspa/src/constants.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "http://pagecheckerapi.azurewebsites.net/api"
-export const BASE_URL = "http://localhost:53321/api"
+export const BASE_URL = "http://pagecheckerapi.azurewebsites.net/api"
+//export const BASE_URL = "http://localhost:53321/api"

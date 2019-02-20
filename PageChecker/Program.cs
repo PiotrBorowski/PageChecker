18053244f0ed7594563f47dfc1215b235a89be8f
LAPTOP-SLRMPIQU\Piotr Borowski <piotrborowski97@op.pl>
2019-02-20 23:47:03 +0100

    deploy to azure unsuccessful TODO:  change SMTP to Azure SendGrid, repair error with facebook auth, install insights

--- PageChecker/Program.cs
+++ PageChecker/Program.cs
@@ -12,6 +12,7 @@ namespace PageCheckerAPI
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
